c04563ad2cd36c2edb6fde0c2c5667cc467f9f6a
Daniel <dmisztal.1297@gmail.com>
2025-03-29 01:30:22 +0100

    nochmal optimierungen: web build auskommentiert

--- server/server.js
+++ server/server.js
@@ -14,7 +14,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+// Falls du derzeit keinen Web-Build hast, kommentiere diese Zeile aus:
+// app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 /**
  * Konvertiert ein Datum in die deutsche Zeitzone ("Europe/Berlin")
